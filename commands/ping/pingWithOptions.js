f2812a935c124ce484584a57159cb4c1b406a7be
Lorenzo Asselinne <lorenzoasselinne@hessautomile.com>
2025-10-03 08:23:53 +0200

    [ADD]command with options

---
+++ commands/ping/pingWithOptions.js
@@ -0,0 +1,22 @@
+//https://discordjs.guide/slash-commands/advanced-creation.html#adding-options
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('optiontest')
+    .setDescription('Tu vas bien ?')
+    .addStringOption(option =>
+      option.setName('input')
+        .setDescription('gépadinsspi')
+        .addChoices(
+          { name: 'oui', value: 'Cool, Osef' },
+          { name: 'non', value: 'Dur, Osef' },
+          { name: 'peut-être', value: 'C\'est une question simple pourtant' },
+        )),
+
+  async execute(interaction) {
+    const input = interaction.options.getString('input');
+    await interaction.reply(`${input}`);
+  }
+}
