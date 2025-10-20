c64a0b4769b68b146ecdcd5b931ffe22a6f74756
Lorenzo Asselinne <lorenzoasselinne@hessautomile.com>
2025-10-20 22:15:38 +0200

    [ADD] message on launch

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,19 @@ const { Events } = require('discord.js');
 module.exports = {
   name: Events.ClientReady,
   once: true,
-  execute(client) {
+  async execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+
+
+    const GENERAL_CHANNEL_ID = '1423020095978082465';
+
+    try {
+      const channel = await client.channels.fetch(GENERAL_CHANNEL_ID);
+      if (channel) {
+        await channel.send('🤖 Bugsy est de retour ! Prêt à répondre à vos questions (ou à râler un peu).');
+      }
+    } catch (error) {
+      console.error('Erreur lors de l\'envoi du message de démarrage:', error);
+    }
   },
 };
